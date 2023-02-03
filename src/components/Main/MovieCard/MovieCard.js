6e77cc4178e95ee185f663743153711b540346a8
Andranik Mkhitaryan <andmkhart77@gmail.com>
2023-02-03 22:06:32 +0400

    Fix in CSS structure of MovieCard

--- src/components/Main/MovieCard/MovieCard.js
+++ src/components/Main/MovieCard/MovieCard.js
@@ -5,13 +5,17 @@ const API_IMG = "https://image.tmdb.org/t/p/w500" ;
 
 function MovieCard ({title,poster_path,release_date,vote_average}) {
     return (
+
         <div className={s.movieCard}>
-            <div className={s.title}>{title}</div>
-            <img className={s.img} src={API_IMG+poster_path}  alt={title}/>
-            <div className={s.aboutMovie}>
-                <div className={s.date}>{release_date}</div>
-                <div className={vote_average>=7?s.rateAqua:s.rate} >{vote_average}</div>
+            <div className={s.movieCardData}>
+                <div className={s.title}>{title}</div>
+                <img className={s.img} src={API_IMG+poster_path}  alt={title}/>
+                <div className={s.aboutMovie}>
+                    <div className={s.date}>{release_date}</div>
+                    <div className={vote_average>=7?s.rateAqua:s.rate} >{vote_average}</div>
+                </div>
             </div>
+
         </div>
     )
 }
